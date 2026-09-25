3ab46c5b6da59451841dc889294c99afe8cceab6
emilianolsotomayor-hash <emiliano.lsotomayor@gmail.com>
2026-09-24 22:19:22 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace SE1
+
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            // 1
+            bool a;
+            int número;
+            // 3 Interpolación
+            a = true;
+            número = 10;
+            Console.WriteLine($"Booleano: {a}");
+            Console.WriteLine($"Número: {número}");
+
+            // 4 Incrementos y decrementos
+            int m = 0;
+            int n = -1;
+            // 5 Operador resto(modúlo)
+            int residuo = 40 % 16;
+            Console.WriteLine($"Residúo: {residuo}");
+            // 6 Operadores aritmeticos
+            double operación = 0;
+        }   operación = (( 30 + 8 -2)/2)*-1;
+    }
+}
